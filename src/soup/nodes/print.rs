ef17edf45f2060c47fa0ac80d109d4ede870e612
Robert Obkircher <robert.obkircher@gmail.com>
2024-08-10 11:57:32 +0200

    refactor printing nodes

--- src/soup/nodes/print.rs
+++ src/soup/nodes/print.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::fmt;
 use std::fmt::Display;
 
@@ -8,17 +9,33 @@ use crate::soup::nodes::{Node, NodeId, Nodes};
 pub struct PrintNodes<'a, 't> {
     node: Option<NodeId>,
     nodes: &'a Nodes<'t>,
+    visited: RefCell<IdSet<NodeId>>,
 }
 
 impl Display for PrintNodes<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut visited = IdSet::zeros(self.nodes.nodes.len());
-        self.nodes.fmt(self.node, f, &mut visited)
+        PrintNodes2 {
+            node: self.node,
+            nodes: self.nodes,
+            visited: &self.visited,
+        }
+        .fmt(f)
     }
 }
+
+pub struct PrintNodes2<'a, 'b, 't> {
+    node: Option<NodeId>,
+    nodes: &'a Nodes<'t>,
+    visited: &'b RefCell<IdSet<NodeId>>,
+}
+
 impl<'t> Nodes<'t> {
     pub(crate) fn print(&self, node: Option<NodeId>) -> PrintNodes {
-        PrintNodes { node, nodes: self }
+        PrintNodes {
+            node,
+            nodes: self,
+            visited: RefCell::new(IdSet::zeros(self.len())),
+        }
     }
 
     // Unique label for graph visualization, e.g. "Add12" or "Region30" or "EQ99"
@@ -28,116 +45,72 @@ impl<'t> Nodes<'t> {
             _ => format!("{}{}", self[node].label(), node),
         }
     }
+}
 
-    fn fmt(
-        &self,
-        node: Option<NodeId>,
-        f: &mut fmt::Formatter,
-        visited: &mut IdSet<NodeId>,
-    ) -> fmt::Result {
-        let Some(node) = node else {
+impl Display for PrintNodes2<'_, '_, '_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Some(node) = self.node else {
             return write!(f, "<?>");
         };
-        visited.add(node);
-        let inputs = &self.inputs[node];
-        match &self[node] {
-            _ if self.is_dead(node) => write!(f, "{}:DEAD", self.unique_name(node)),
-            Node::Return => {
-                write!(f, "return ")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, ";")
-            }
-            Node::Add => {
-                write!(f, "(")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, "+")?;
-                self.fmt(inputs[2], f, visited)?;
-                write!(f, ")")
-            }
+        self.visited.borrow_mut().add(node);
+
+        let nodes = self.nodes;
+        let inputs = &nodes.inputs[node];
+
+        let print = |node| Self {
+            node,
+            nodes,
+            visited: self.visited,
+        };
+        let input = |index| print(inputs[index]);
+        let mut binary = |op: &str| write!(f, "({}{}{})", input(1), op, input(2));
+
+        match &nodes[node] {
+            _ if nodes.is_dead(node) => write!(f, "{}:DEAD", nodes.unique_name(node)),
+            Node::Add => binary("+"),
+            Node::Sub => binary("-"),
+            Node::Mul => binary("*"),
+            Node::Div => binary("/"),
+            Node::Bool(op) => binary(op.str()),
+            Node::Return => write!(f, "return {};", input(1)),
             Node::Constant(ty) => write!(f, "{}", ty),
             n @ Node::Start { .. } => write!(f, "{}", n.label()),
-            Node::Sub => {
-                write!(f, "(")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, "-")?;
-                self.fmt(inputs[2], f, visited)?;
-                write!(f, ")")
-            }
-            Node::Mul => {
-                write!(f, "(")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, "*")?;
-                self.fmt(inputs[2], f, visited)?;
-                write!(f, ")")
-            }
-            Node::Div => {
-                write!(f, "(")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, "*")?;
-                self.fmt(inputs[2], f, visited)?;
-                write!(f, ")")
-            }
-            Node::Minus => {
-                write!(f, "(-")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, ")")
-            }
+            Node::Minus => write!(f, "(-{})", input(1)),
             n @ Node::Scope(scope) => {
                 write!(f, "{}", n.label())?;
                 for s in &scope.scopes {
                     write!(f, "[")?;
-                    for (i, (name, input)) in s.iter().enumerate() {
+                    for (i, (name, index)) in s.iter().enumerate() {
                         if i > 0 {
                             write!(f, ", ")?;
                         }
-                        write!(f, "{name}:")?;
-                        self.fmt(inputs[*input], f, visited)?;
+                        write!(f, "{name}:{}", input(*index))?;
                     }
                     write!(f, "]")?;
                 }
                 Ok(())
             }
-            Node::Bool(op) => {
-                write!(f, "(")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, "{}", op.str())?;
-                self.fmt(inputs[2], f, visited)?;
-                write!(f, ")")
-            }
-            Node::Not => {
-                write!(f, "(!")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, ")")
-            }
-            Node::Proj(proj) => {
-                write!(f, "{}", proj.label)
-            }
-            Node::If => {
-                write!(f, "if( ")?;
-                self.fmt(inputs[1], f, visited)?;
-                write!(f, " )")
-            }
+            Node::Not => write!(f, "(!{})", input(1)),
+            Node::Proj(proj) => write!(f, "{}", proj.label),
+            Node::If => write!(f, "if( {} )", input(1)),
             Node::Phi(_) => {
                 write!(f, "Phi(")?;
                 for (i, input) in inputs.iter().enumerate() {
                     if i > 0 {
                         write!(f, ",")?;
                     }
-                    self.fmt(*input, f, visited)?;
+                    write!(f, "{}", print(*input))?;
                 }
                 write!(f, ")")
             }
-            n @ Node::Region => {
-                write!(f, "{}{}", n.label(), node.index())
-            }
+            n @ Node::Region => write!(f, "{}{}", n.label(), node.index()),
             Node::Stop => {
-                if let Some(ret) = self.unique_input(node) {
-                    self.fmt(Some(ret), f, visited)
+                if let ret @ Some(_) = nodes.unique_input(node) {
+                    write!(f, "{}", print(ret))
                 } else {
                     write!(f, "Stop[ ")?;
                     for ret in inputs {
-                        self.fmt(*ret, f, visited)?;
-                        write!(f, " ")?;
+                        write!(f, "{} ", print(*ret))?;
                     }
                     write!(f, "]")
                 }
