40adf4e6807f765d4e4b5e27d45a8d0c2625a40b
Robert Obkircher <robert.obkircher@gmail.com>
2024-08-17 09:13:52 +0200

    deleted old code to avoid confusion

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,5 @@
 #![feature(hash_raw_entry)]
 #![feature(get_many_mut)]
 
-use std::path::PathBuf;
-
 pub mod datastructures;
 pub mod sea_of_nodes;
-
-#[derive(Debug, Eq, PartialEq)]
-pub enum ExitCode {
-    Success = 0,
-    Failure,
-    Usage,
-    InvalidFile,
-}
-
-pub fn run(main: PathBuf, _build_args: Vec<String>) -> ExitCode {
-    if !main.is_file() || main.extension().map(|it| it != "ro").unwrap_or(true) {
-        eprintln!("Not a .ro file: '{}'", main.to_string_lossy());
-        return ExitCode::InvalidFile;
-    }
-
-    match compile(main, _build_args) {
-        Ok(_) => ExitCode::Success,
-        Err(_) => ExitCode::Failure,
-    }
-}
-
-pub fn compile(_build_file: PathBuf, _build_args: Vec<String>) -> Result<PathBuf, ()> {
-    todo!()
-}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
 fn main() {
-    let exit_code = {
-        let mut args = std::env::args();
-        let executable = args.next().unwrap();
-
-        if let Some(file) = args.next() {
-            ro::run(file.into(), args.collect())
-        } else {
-            eprintln!("Usage: {} build.ro [build_args...]", executable);
-            ro::ExitCode::Usage
-        }
-    };
-
-    std::process::exit(exit_code as i32);
+    todo!()
 }
