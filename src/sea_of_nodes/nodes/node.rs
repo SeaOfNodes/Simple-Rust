dc1c030daa0940db7beaa9eeee38c2f5daa15fbd
Robert Obkircher <robert.obkircher@gmail.com>
2025-01-16 16:53:11 +0100

    get rid of downcasts in constructors

--- src/sea_of_nodes/nodes/node.rs
+++ src/sea_of_nodes/nodes/node.rs
@@ -465,21 +465,21 @@ impl<'t> Op<'t> {
 impl Start {
     pub fn new<'t>(args: &[Ty<'t>], sea: &mut Nodes<'t>) -> Self {
         let args = sea.types.get_tuple_from_slice(args);
-        let this = sea.create((
+        let this = Start(sea.create((
             Op::Start(StartOp {
                 args,
                 alias_starts: HashMap::new(),
             }),
             vec![],
-        ));
+        )));
         sea.ty[this] = Some(args);
-        this.to_start(sea).unwrap()
+        this
     }
 }
 
 impl Return {
     pub fn new(ctrl: Node, data: Node, scope: Option<Scope>, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Return, vec![Some(ctrl), Some(data)]));
+        let this = Return(sea.create((Op::Return, vec![Some(ctrl), Some(data)])));
 
         if let Some(scope) = scope {
             // We lookup memory slices by the naming convention that they start with $
@@ -493,74 +493,64 @@ impl Return {
             }
         }
 
-        this.to_return(sea).unwrap()
+        this
     }
 }
 impl Constant {
     pub fn new<'t>(ty: Ty<'t>, sea: &mut Nodes<'t>) -> Self {
         let start = sea.start;
-        let this = sea.create((Op::Constant(ty), vec![Some(*start)]));
-        this.to_constant(sea).unwrap()
+        Constant(sea.create((Op::Constant(ty), vec![Some(*start)])))
     }
 }
 impl XCtrl {
     pub fn new(sea: &mut Nodes) -> Self {
         let start = sea.start;
-        let this = sea.create((Op::XCtrl, vec![Some(*start)]));
-        this.to_xctrl(sea).unwrap()
+        XCtrl(sea.create((Op::XCtrl, vec![Some(*start)])))
     }
 }
 
 impl Add {
     pub fn new(left: Node, right: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Add, vec![None, Some(left), Some(right)]));
-        this.to_add(sea).unwrap()
+        Add(sea.create((Op::Add, vec![None, Some(left), Some(right)])))
     }
 }
 impl Sub {
     pub fn new(left: Node, right: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Sub, vec![None, Some(left), Some(right)]));
-        this.to_sub(sea).unwrap()
+        Sub(sea.create((Op::Sub, vec![None, Some(left), Some(right)])))
     }
 }
 impl Mul {
     pub fn new(left: Node, right: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Mul, vec![None, Some(left), Some(right)]));
-        this.to_mul(sea).unwrap()
+        Mul(sea.create((Op::Mul, vec![None, Some(left), Some(right)])))
     }
 }
 impl Div {
     pub fn new(left: Node, right: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Div, vec![None, Some(left), Some(right)]));
-        this.to_div(sea).unwrap()
+        Div(sea.create((Op::Div, vec![None, Some(left), Some(right)])))
     }
 }
 
 impl Minus {
     pub fn new(expr: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Minus, vec![None, Some(expr)]));
-        this.to_minus(sea).unwrap()
+        Minus(sea.create((Op::Minus, vec![None, Some(expr)])))
     }
 }
 
 impl Scope {
     pub fn new(sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Scope(ScopeOp { scopes: vec![] }), vec![]));
-        this.to_scope(sea).unwrap()
+        Scope(sea.create((Op::Scope(ScopeOp { scopes: vec![] }), vec![])))
     }
 }
 
 impl Bool {
     pub fn new(left: Node, right: Node, op: BoolOp, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Bool(op), vec![None, Some(left), Some(right)]));
-        this.to_bool(sea).unwrap()
+        Bool(sea.create((Op::Bool(op), vec![None, Some(left), Some(right)])))
     }
 }
 
 impl Not {
     pub fn new(expr: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Not, vec![None, Some(expr)]));
-        this.to_not(sea).unwrap()
+        Not(sea.create((Op::Not, vec![None, Some(expr)])))
     }
 }
 
@@ -571,8 +561,7 @@ impl Proj {
         label: &'t str,
         sea: &mut Nodes<'t>,
     ) -> Self {
-        let this = sea.create((Op::Proj(ProjOp { index, label }), vec![Some(ctrl.into())]));
-        this.to_proj(sea).unwrap()
+        Proj(sea.create((Op::Proj(ProjOp { index, label }), vec![Some(ctrl.into())])))
     }
 }
 
@@ -583,8 +572,7 @@ impl CProj {
         label: &'t str,
         sea: &mut Nodes<'t>,
     ) -> Self {
-        let this = sea.create((Op::CProj(ProjOp { index, label }), vec![Some(ctrl.into())]));
-        this.to_cproj(sea).unwrap()
+        CProj(sea.create((Op::CProj(ProjOp { index, label }), vec![Some(ctrl.into())])))
     }
 }
 
@@ -594,9 +582,9 @@ impl If {
             Some(n) => (IfOp::Cond, n),
             None => (IfOp::Never, *sea.zero),
         };
-        let this = sea.create((Op::If(op), vec![Some(ctrl), Some(pred)]));
-        sea.iter_peeps.add(this); // Because idoms are complex, just add it
-        this.to_if(sea).unwrap()
+        let this = If(sea.create((Op::If(op), vec![Some(ctrl), Some(pred)])));
+        sea.iter_peeps.add(*this); // Because idoms are complex, just add it
+        this
     }
 }
 
@@ -607,32 +595,27 @@ impl Phi {
         inputs: Vec<Option<Node>>,
         sea: &mut Nodes<'t>,
     ) -> Self {
-        let this = sea.create((Op::Phi(PhiOp { label, ty }), inputs));
-        this.to_phi(sea).unwrap()
+        Phi(sea.create((Op::Phi(PhiOp { label, ty }), inputs)))
     }
 }
 impl Region {
     pub fn new(inputs: Vec<Option<Node>>, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Region, inputs));
-        this.to_region(sea).unwrap()
+        Region(sea.create((Op::Region, inputs)))
     }
 }
 impl Stop {
     pub fn new(sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Stop, vec![]));
-        this.to_stop(sea).unwrap()
+        Stop(sea.create((Op::Stop, vec![])))
     }
 }
 impl Loop {
     pub fn new(entry: Node, sea: &mut Nodes) -> Self {
-        let this = sea.create((Op::Loop, vec![None, Some(entry), None]));
-        this.to_loop(sea).unwrap()
+        Loop(sea.create((Op::Loop, vec![None, Some(entry), None])))
     }
 }
 impl Cast {
     pub fn new<'t>(ty: Ty<'t>, ctrl: Node, i: Node, sea: &mut Nodes<'t>) -> Self {
-        let this = sea.create((Op::Cast(ty), vec![Some(ctrl), Some(i)]));
-        this.to_cast(sea).unwrap()
+        Cast(sea.create((Op::Cast(ty), vec![Some(ctrl), Some(i)])))
     }
 }
 
@@ -644,15 +627,14 @@ impl Load {
         [mem_slice, mem_ptr]: [Node; 2],
         sea: &mut Nodes<'t>,
     ) -> Self {
-        let this = sea.create((
+        Load(sea.create((
             Op::Load(LoadOp {
                 name,
                 alias,
                 declared_type,
             }),
             vec![None, Some(mem_slice), Some(mem_ptr)],
-        ));
-        this.to_load(sea).unwrap()
+        )))
     }
 }
 impl Store {
@@ -662,16 +644,14 @@ impl Store {
         [ctrl, mem_slice, mem_ptr, value]: [Node; 4],
         sea: &mut Nodes<'t>,
     ) -> Self {
-        let this = sea.create((
+        Store(sea.create((
             Op::Store(StoreOp { name, alias }),
             vec![Some(ctrl), Some(mem_slice), Some(mem_ptr), Some(value)],
-        ));
-        this.to_store(sea).unwrap()
+        )))
     }
 }
 impl New {
     pub fn new<'t>(ptr: Ty<'t>, ctrl: Node, sea: &mut Nodes<'t>) -> Self {
-        let this = sea.create((Op::New(ptr), vec![Some(ctrl)]));
-        this.to_new(sea).unwrap()
+        New(sea.create((Op::New(ptr), vec![Some(ctrl)])))
     }
 }
